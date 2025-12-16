8636fab9b9ca510df8571253e75e142aefeafef0
shrage <shreyansh3536@gmail.com>
2025-12-16 23:27:37 +0530

    title favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,6 +9,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/png/appicon.png" type="image/png" />
+        <title>Jankalyan</title>
+        <meta name="description" content="Jankalyan Admin Application" />
       </head>
       <body className={`${inter.variable} antialiased font-display`}>
           {children}
